5b8c322da41090d97ea61673bcf7659dec8635c7
Jesus Fernandez <jesus_h_tlv@outlook.com>
2024-10-26 18:03:38 -0500

    Proyecto base gestion pedidos backend - Jesus Fernandez

---
+++ src/main/java/com/jfernandez/gestion/pedidos/GestionPedidosBackendApplication.java
@@ -0,0 +1,13 @@
+package com.jfernandez.gestion.pedidos;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GestionPedidosBackendApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GestionPedidosBackendApplication.class, args);
+	}
+
+}
